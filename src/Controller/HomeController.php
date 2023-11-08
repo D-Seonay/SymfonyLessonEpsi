7473486e1787bf44239e6409fa8e8e3ff9452eb9
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-08 12:37:53 +0100

    Add routes and methods for homepage and "bonjour" page.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Contrôleur pour la page d'accueil et la page "bonjour".
+ */
 class HomeController extends AbstractController
 {
+    /**
+     * Affiche la page d'accueil.
+     * 
+     * @return Response
+     */
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
@@ -15,6 +23,12 @@ class HomeController extends AbstractController
             'controller_name' => 'Seonay',
         ]);
     }
+
+    /**
+     * Affiche la page "bonjour" avec les informations de la personne et la date du jour.
+     * 
+     * @return Response
+     */
     #[Route('/bonjour', name: 'app_bonjour')]
     public function app_bonjour (): Response
     {
@@ -26,6 +40,7 @@ class HomeController extends AbstractController
             'job' => 'Developpeur Fullstack',
             'pays' => 'France',
         ];
+        
         return $this->render('home/hello.html.twig', [
             'prenom' => $personne['prenom'],
             'nom' => $personne['nom'],
@@ -36,6 +51,26 @@ class HomeController extends AbstractController
 
         ]);
     }
+    #[Route('/bonjourDynamique/{prenom}/age/{age}', name:'app_bonjourDynamique')]
+    public function bonojourDynamique(string $prenom, $age): Response
+    {
+        $dateDuJour = date('d/m/Y H:i:s');
+        $personne = [
+            'prenom' => $prenom,
+            'nom' => 'D',
+            'age' => $age,
+            'job' => 'Developpeur Fullstack',
+            'pays' => 'France',
+        ];
+        
+        return $this->render('home/hello.html.twig', [
+            'prenom' => $personne['prenom'],
+            'nom' => $personne['nom'],
+            'age' => $personne['age'],
+            'job' => $personne['job'],
+            'pays' => $personne['pays'],
+            'dateDuJour' => $dateDuJour,
 
-
+        ]);
+    }
 }
