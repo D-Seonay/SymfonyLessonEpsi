72007acba97f862924707fbdea16fa17ba8ff0af
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-08 12:07:05 +0100

    Update HomeController and index template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,13 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'Seonay',
+        ]);
+    }
+    #[Route('/bonjour', name: 'app_bonjour')]
+    public function app_bonjour (): Response
+    {
+        return $this->render('home/hello.html.twig', [
         ]);
     }
 }
